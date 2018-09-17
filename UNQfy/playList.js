70b9ac9d3a33b4972a93f679b0d8ee0298297895
CarlosSalda <42792932+CarlosSalda@users.noreply.github.com>
2018-09-17 18:48:21 -0300

    Add files via upload

---
+++ UNQfy/playList.js
@@ -0,0 +1,53 @@
+/*                                                PlayList                                                             */ 
+const Track = require("Track");
+
+
+var tracks= [];
+var namePlayList = "";
+
+class Playlist{
+    constructor (namePlayList, tracks){
+        this.namePlayList= namePlayList;
+        this.tracks = tracks;
+    }
+    
+    /*cuenta la duracion de toda la PlayList, la misma es la suma de la duracion de todo los tracks que la componen */
+    durationPlayList (){
+    let count= this.tracks.length;
+    let durationPL = 0;
+    for(let i=0; i < count;i++) {
+        durationPL += this.tracks[i].duration;
+    }
+    return durationPL;
+    }
+    
+    /*retorna bool con respecto a la PlayList que se pregunta si tiene,
+      el track que se pasa como parametro */
+    /*track pasado como parametro se espera que sea de la clase Track */
+    hasTrack(track){
+       return this.tracks.includes(track);
+    }
+
+    /*saca de la lista de tracks de la PlayList,
+      solo si lo encuentra, al track que se pasa como parametro */
+    
+    /*track pasado como parametro se espera que sea de la clase Track */
+    removeTrack(track){
+        let indexToDelete = this.tracks.indexOf(track) ;
+        if(indexToDelete > -1){
+            this.tracks.splice(indexToDelete,1);
+        }
+     }
+    /*elimina a todos los tracks pasados por parametro */
+     filterTrack(trackArray){
+       let count=trackArray.length;
+        for(let i=0; i<count; i++){
+          this.removeTrack(trackArray[i]);
+        }
+     }
+     
+     /*retorna todos los tracks de la playlist(los retorna como objetos de la clase Track)*/
+     getTracks(){
+       return this.tracks;
+     }
+}
---
+++ UNQfy/track.js
@@ -0,0 +1,55 @@
+var genres = [];
+var id = 0;
+var name = "";
+var duration = 0;
+var author = "";
+
+class Track{
+    constructor(id, name, duration, genres, author){
+        this.id = id;
+        this.name = name;
+        this.duration = duration;
+        this.genres = genres;
+        this.author = author;
+    }
+
+    /*agrega un nuevo genero al Track */
+    addGenre(newGenre){
+      if(this.genres.indexOf(newGenre) == -1){
+        this.genres.push(newGenre);
+      }
+    }
+
+    /*elimina el genero pasado como parametro como genreToDelete */
+    removeGenre(genreToDelete){
+    let indexToDelete = this.genres.indexOf(genreToDelete) ;
+      if(indexToDelete > -1){
+        this.genres.splice(indexToDelete,1);
+      }
+    } 
+
+    /*retorna si el genero existe en el track*/
+    hasGenre(genre){
+      return this.genres.includes(genre);
+    }
+
+    /*retorna si el codigo que se pasa, pertenece al Id de la clase Track en el que se invoca */
+    isTrack(codigo){
+      return this.id == codigo; 
+    }
+
+    /*retorna bool si el artist del parametro, es el author del Track */
+    hasArtist(artist){
+     return this.author == artist;
+    }
+
+    /*retorna un bool que corresponde si la duracion del track es menor a los "segundos" pasados por parametro*/
+    hasMaxDuration(segundos){
+      return this.duration < segundos;
+    }
+}
+
+
+
+
+
